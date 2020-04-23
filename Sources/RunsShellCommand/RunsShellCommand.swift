47c1e493a944716cfa8dbe57c5e75f637aaab033
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-04-24 04:05:09 +0530

    add documentation

--- Sources/RunsShellCommand/RunsShellCommand.swift
+++ Sources/RunsShellCommand/RunsShellCommand.swift
@@ -10,18 +10,40 @@ import ShellOut
 
 public protocol AnyShellCommand {
     
-    var command: String { get }
-    var arguments: [String] { get }
-    var successMessage: String? { get }
+    /// the primary part of the command (the firs word of command)
+    var command: String { get set }
 
-    func execute(onSuccess: ((String) -> Void)?, onFailure: ((ShellOutError) -> Void)?)
+    /// default arguments passed during execution before the additional arguments, default: empty
+    var defaultArguments: [String] { get set }
+
+    /// success message that, if not nil, gets printed by default when the command is successfully executed, default: nil
+    /// if there is a custom handling for onSuccess block in execute method then this variable is not used
+    var successMessage: String? { get set }
+
+    
+    /// executes the command with its default arguments + the arguments, if any provided as parameter
+    /// - Parameters:
+    ///   - arguments: an array of strings, default: empty
+    ///   - onSuccess: for handling successful command execution, the output of the command is received on callback, default: prints the output
+    ///   - onFailure: handle any failure in execution, returns the error, default: prints error message (STDERR) and output (STDOUT)
+    func execute(with arguments: [String], onSuccess: ((String) -> Void)?, onFailure: ((ShellOutError) -> Void)?)
 }
 
 public extension AnyShellCommand {
     
-    func execute(onSuccess: ((String) -> Void)? = nil, onFailure: ((ShellOutError) -> Void)? = nil) {
+    var defaultArguments: [String] {
+        get { return [] }
+        set {}
+    }
+    
+    var successMessage: String? {
+        get { return nil }
+        set {}
+    }
+    
+    func execute(with arguments: [String] = [], onSuccess: ((String) -> Void)? = nil, onFailure: ((ShellOutError) -> Void)? = nil) {
         do {
-            let output = try shellOut(to: command, arguments: arguments)
+            let output = try shellOut(to: command, arguments: defaultArguments + arguments)
             if let onSuccess = onSuccess {
                 onSuccess(output)
             } else {
