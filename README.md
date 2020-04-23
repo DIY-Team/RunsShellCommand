271615362a67107e832eb9da881d8309489d94b2
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-04-24 04:07:41 +0530

    Update readme (#2)
    * update read me: include credits and usage example
    
    * update read me: add installation info
    
    * add documentation
    
    * add example code in library file

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# ss-runs-shell-command
+# RunsShellCommand
 A library to support defining and running shell commands in swift scripts. Provides a protocol to define the command's composition and a method to execute it.
+
+## Special Mention
+The library uses John Sundell's Shellout repo as depedency for actually running the shell commands.
+More info:
+`https://github.com/JohnSundell/ShellOut.git`
+
+## Usage
+The library provides a protocol `AnyShellCommand`. 
+Use this protocol to define the structure of your command's class, struct or enum. 
+
+### Example 1:
+```
+enum GitCommand: AnyShellCommand {
+    case createAndCheckoutNewBranch
+    case deleteBranch
+    
+    var command: String {
+        get { return "git" }
+        set {}
+    }
+    
+    var defaultArguments: [String] {
+        get {
+            switch self {
+            case.createAndCheckoutNewBranch: return ["checkout", "-b"]
+            case .deleteBranch: return ["branch", "-d"]
+            }
+        }
+        set {}
+    }
+}
+
+func demo1() {
+    GitCommand.createAndCheckoutNewBranch.execute(with: ["new-branch-name"])
+    GitCommand.deleteBranch.execute(with: ["some-old-branch"])
+}
+```
+
+### Example 2:
+```
+struct FileOperationCommand: AnyShellCommand {
+    var command: String
+}
+
+func demo2() {
+    let cDCommand = FileOperationCommand(command: "cd")
+
+    let folderName = "new-folder"
+    FileOperationCommand(command: "mkdir").execute(with: [folderName], onSuccess: { _ in
+        cDCommand.execute(with: [folderName])
+    }, onFailure: { _ in
+        print("Failed to create new folder.")
+    })
+}
+
+```
+
+## Installation
+
+
+### Using SwiftPM
+In `Package.swift` file, define under dependencies:
+`.package(url: "https://github.com/DIY-Team/RunsShellCommand", from: 0.0.1)`
+dependency name: `RunsShellCommand`
--- Sources/RunsShellCommand/RunsShellCommand.swift
+++ Sources/RunsShellCommand/RunsShellCommand.swift
@@ -10,18 +10,40 @@ import ShellOut
 
 public protocol AnyShellCommand {
     
-    var command: String { get }
-    var arguments: [String] { get }
-    var successMessage: String? { get }
+    /// the primary part of the command (the firs word of command)
+    var command: String { get set }
 
-    func execute(onSuccess: ((String) -> Void)?, onFailure: ((ShellOutError) -> Void)?)
+    /// default arguments passed during execution before the additional arguments, default: empty
+    var defaultArguments: [String] { get set }
+
+    /// success message that, if not nil, gets printed by default when the command is successfully executed, default: nil
+    /// if there is a custom handling for onSuccess block in execute method then this variable is not used
+    var successMessage: String? { get set }
+
+    
+    /// executes the command with its default arguments + the arguments, if any provided as parameter
+    /// - Parameters:
+    ///   - arguments: an array of strings, default: empty
+    ///   - onSuccess: for handling successful command execution, the output of the command is received on callback, default: prints the output
+    ///   - onFailure: handle any failure in execution, returns the error, default: prints error message (STDERR) and output (STDOUT)
+    func execute(with arguments: [String], onSuccess: ((String) -> Void)?, onFailure: ((ShellOutError) -> Void)?)
 }
 
 public extension AnyShellCommand {
     
-    func execute(onSuccess: ((String) -> Void)? = nil, onFailure: ((ShellOutError) -> Void)? = nil) {
+    var defaultArguments: [String] {
+        get { return [] }
+        set {}
+    }
+    
+    var successMessage: String? {
+        get { return nil }
+        set {}
+    }
+    
+    func execute(with arguments: [String] = [], onSuccess: ((String) -> Void)? = nil, onFailure: ((ShellOutError) -> Void)? = nil) {
         do {
-            let output = try shellOut(to: command, arguments: arguments)
+            let output = try shellOut(to: command, arguments: defaultArguments + arguments)
             if let onSuccess = onSuccess {
                 onSuccess(output)
             } else {
@@ -41,3 +63,45 @@ public extension AnyShellCommand {
         }
     }
 }
+
+enum GitCommand: AnyShellCommand {
+    case createAndCheckoutNewBranch
+    case deleteBranch
+    
+    var command: String {
+        get { return "git" }
+        set {}
+    }
+    
+    var defaultArguments: [String] {
+        get {
+            switch self {
+            case.createAndCheckoutNewBranch: return ["checkout", "-b"]
+            case .deleteBranch: return ["branch", "-d"]
+            }
+        }
+        set {}
+    }
+}
+
+func demo1() {
+    GitCommand.createAndCheckoutNewBranch.execute(with: ["new-branch-name"])
+    GitCommand.deleteBranch.execute(with: ["some-old-branch"])
+}
+
+struct FileOperationCommand: AnyShellCommand {
+    
+    var command: String
+}
+
+func demo2() {
+    let cDCommand = FileOperationCommand(command: "cd")
+
+    let folderName = "new-folder"
+    FileOperationCommand(command: "mkdir").execute(with: [folderName], onSuccess: { _ in
+        cDCommand.execute(with: [folderName])
+    }, onFailure: { _ in
+        print("Failed to create new folder.")
+    })
+}
+
